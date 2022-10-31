df7d85ed2be940f784b99f3267382717a7f27e6f
Eric Dundas <kuribodragon@gmail.com>
2022-10-30 20:16:40 -0500

    started working on enemy movements

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour //change later
 {
 
+    public enum enemyState
+    {
+        WANDERING,
+        CHASING,
+        INVESTIGATING,
+    }
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyState currentState = enemyState.WANDERING;
     }
 
     // Update is called once per frame
@@ -21,5 +32,11 @@ public class EnemyMovement : MonoBehaviour //change later
     {
         //find closest player
         //move towards them
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != null)
+            return;
     }
 }
