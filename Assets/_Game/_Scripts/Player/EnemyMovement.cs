6da8e45eb1139845721f5631522ea05d73de26fe
Eric Dundas <kuribodragon@gmail.com>
2022-12-02 23:05:10 -0600

    added more network components

--- Assets/_Game/_Scripts/Player/EnemyMovement.cs
+++ Assets/_Game/_Scripts/Player/EnemyMovement.cs
@@ -18,32 +18,60 @@ public class EnemyMovement : MonoBehaviour //change later
     public float sightRadius = 5;
     private Vector3 lastPlayerPosition;
     enemyState currentState;
+    public Animator animator;
+    private int routeChecker = 25;
 
     // Start is called before the first frame update
     void Start()
     {
         currentState = enemyState.WANDERING;
-    }
+		agent.stoppingDistance = 2f;
+	}
 
     // Update is called once per frame
     void Update()
     {
-        Collider[] inRangePlayers = Physics.OverlapSphere(this.transform.position, sightRadius, whatIsPlayer);
-        if (inRangePlayers.Length > 0)
+        if (currentState == enemyState.WANDERING)
         {
-            player = inRangePlayers[0].transform;
-            currentState = enemyState.CHASING;
-            lastPlayerPosition = player.position;
-            MoveTowardsPlayer(lastPlayerPosition);
-        } else if (currentState == enemyState.CHASING)
-        {
-            currentState=enemyState.INVESTIGATING;
-            lastPlayerPosition = player.position;
-            MoveTowardsPlayer(lastPlayerPosition);
+            animator.SetBool("WANDERING", true);
         }
+        else {
+            animator.SetBool("WANDERING", false);
+        }
+		if (agent.isStopped || agent.remainingDistance <= agent.stoppingDistance)
+		{
+            animator.SetBool("isAttacking", true);
+		}
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+		{
+			animator.SetBool("isAttacking", false);
+		}
     }
 
-    void MoveTowardsPlayer(Vector3 TargetPosition)
+	private void FixedUpdate()
+	{
+		if(routeChecker < 0)
+        {
+			Collider[] inRangePlayers = Physics.OverlapSphere(this.transform.position, sightRadius, whatIsPlayer);
+			if (inRangePlayers.Length > 0)
+			{
+				player = inRangePlayers[0].transform;
+				currentState = enemyState.CHASING;
+				lastPlayerPosition = player.position;
+				MoveTowardsPlayer(lastPlayerPosition);
+			}
+			else if (currentState == enemyState.CHASING)
+			{
+				currentState = enemyState.INVESTIGATING;
+				lastPlayerPosition = player.position;
+				MoveTowardsPlayer(lastPlayerPosition);
+			}
+            routeChecker = 25;
+		}
+        routeChecker--;
+	}
+
+	void MoveTowardsPlayer(Vector3 TargetPosition)
     {
         agent.SetDestination(TargetPosition);
 
